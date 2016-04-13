c04645f2b2d91e11d420678855405cf5a434ee94
Filip Johansson <filip.carl.johansson@gmail.com>
2016-04-13 15:34:15 +0200

    changed n to id

--- client/js/controllers/formCtrl.js
+++ client/js/controllers/formCtrl.js
@@ -5,32 +5,32 @@ atApp.controller('FormCtrl', function ($scope, $sce) {
 			q : "What is your name?",
 			t : "input",
 			a : ["bla 1", "bla 2", "bla 3"],
-			n : "1"
+			id : "1"
 		},
 		{
 			q : "What is your age?",
 			t : "select",
 			a : ["18-20", "20-25", "25-30"],
-			n : "2"
+			id: "2"
 		},
 		{
 			q : "How satisfied are you?",
 			t : "radio",
 			a : [">(",":(", ":|", ":)", ":D"],
-			n : "3"
+			id: "3"
 		},
 		{
 			q : "What is an arduino?",
 			t : "check",
 			a : ["motor", "processor", "micro controller", "Small computer"],
-			n : "4"
+			id: "4"
 		},
 	];
 
 	$scope.templates = {
 		select : "<select><option ng-repeat='a in q.a'>{{a}}</option></select>",
 		input  : "<input></input>",
-		radio  : "<form><div ng-repeat='a in q.a'><input type='radio' name='{{q.n}}'>{{a}}<br></div></form>",
+		radio  : "<form><div ng-repeat='a in q.a'><input type='radio' name='{{q.id}}'>{{a}}<br></div></form>",
 		check  : "<div ng-repeat='a in q.a'><input type='checkbox'>{{a}}<br></div> "
 	}
 
