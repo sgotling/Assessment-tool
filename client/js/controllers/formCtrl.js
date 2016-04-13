65d8c9f4155deffb65d656f1a7233218b9282468
Filip Johansson <filip.carl.johansson@gmail.com>
2016-04-13 15:05:50 +0200

    Working fine =)

--- client/js/controllers/formCtrl.js
+++ client/js/controllers/formCtrl.js
@@ -4,29 +4,33 @@ atApp.controller('FormCtrl', function ($scope, $sce) {
 		{
 			q : "What is your name?",
 			t : "input",
-			a : ["bla 1", "bla 2", "bla 3"]
+			a : ["bla 1", "bla 2", "bla 3"],
+			n : "1"
 		},
 		{
 			q : "What is your age?",
 			t : "select",
-			a : ["18-20", "20-25", "25-30"]
+			a : ["18-20", "20-25", "25-30"],
+			n : "2"
 		},
 		{
 			q : "How satisfied are you?",
 			t : "radio",
-			a : [">(",":(", ":|", ":)", ":D"]
+			a : [">(",":(", ":|", ":)", ":D"],
+			n : "3"
 		},
 		{
 			q : "What is an arduino?",
 			t : "check",
-			a : ["motor", "processor", "micro controller", "Small computer"]
-		}
+			a : ["motor", "processor", "micro controller", "Small computer"],
+			n : "4"
+		},
 	];
 
 	$scope.templates = {
 		select : "<select><option ng-repeat='a in q.a'>{{a}}</option></select>",
 		input  : "<input></input>",
-		radio  : "<div ng-repeat='a in q.a'><input type='radio'>{{a}}<br></div>",
+		radio  : "<form><div ng-repeat='a in q.a'><input type='radio' name='{{q.n}}'>{{a}}<br></div></form>",
 		check  : "<div ng-repeat='a in q.a'><input type='checkbox'>{{a}}<br></div> "
 	}
 
