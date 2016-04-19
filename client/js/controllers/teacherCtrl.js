44246bd81bcfe934ea655626e5822be0166e2ceb
sgotling <simon.gotling@hotmail.com>
2016-04-19 15:42:05 +0200

    You can now add forms with questions.

--- client/js/controllers/teacherCtrl.js
+++ client/js/controllers/teacherCtrl.js
@@ -1,17 +1,41 @@
 atApp.controller('teacherCtrl', function ($scope, DB) {
-	// alert();
-	$scope.qs = DB.getQs();
+	$scope.f = {};
+	$scope.f.q = [];
 	$scope.q = {};
 	$scope.q.a = [];
 	$scope.types=["select", "input", "radio", "check"];
+	$scope.var = {
+		newQ: false,
+		fs:[],
+		f: {},
+		
+	}
+	$scope.var.f.q = []
+
+	
+
+	$scope.addForm = function (f) {
+		var fCopy = angular.copy(f);
+		console.log($scope.f);
+		$scope.var.fs.push(f);
+	}
 
 	$scope.addQuestion = function (q) {
 		var qCopy = angular.copy(q);
-		DB.setQs(q);
+		console.log(qCopy)
+		$scope.var.f.q.push(q);
+		// $scope.newQ=false;
+
+	}
+
+	$scope.clearF = function () {
+		$scope.var.f = {};
+		$scope.var.f.q = [];
+	}
+
+	$scope.clearQ = function () {
 		$scope.q = {};
 		$scope.q.a = [];
-		console.log($scope.qs);
-
 	}
 	
 	$scope.addAnswer = function (a) {
@@ -21,5 +45,13 @@ atApp.controller('teacherCtrl', function ($scope, DB) {
 
 	$scope.remove = function (array, index) {
 		array.splice(index, 1);
+	}
+	$scope.showAns = function (t) {
+		if (t != "input" && t != undefined) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 });
