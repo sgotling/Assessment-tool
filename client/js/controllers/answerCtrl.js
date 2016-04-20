769ebe0a94836d2559d3961859b3f36e2bfa1b7a
Filip Johansson <filip.carl.johansson@gmail.com>
2016-04-20 21:33:35 +0200

    Putsat upp designen lite bara

--- client/js/controllers/answerCtrl.js
+++ client/js/controllers/answerCtrl.js
@@ -5,9 +5,9 @@ atApp.controller('AnswerCtrl', function ($scope, $sce, AT, $location) {
 	$scope.answers = {};
 	$scope.templates = {
 		select : "<select ng-model='answers[q.q]'><option ng-repeat='a in q.a'>{{a}}</option></select>",
-		input  : "<input ng-model='answers[q.q]'></input>",
-		radio  : "<div ng-repeat='a in q.a'><input type='radio' name='{{q.q}}' ng-model='answers[q.q]' ng-value='q.a[$index]'>{{a}}<br></div>",
-		check  : "<div ng-repeat='a in q.a'><input type='checkbox' ng-model='answers[q.q][a]'>{{a}}<br></div> "
+		input  : "<input type='text' ng-model='answers[q.q]'></input>",
+		radio  : "<div ng-repeat='a in q.a'><input type='radio' name='{{q.q}}' ng-model='answers[q.q]' ng-value='q.a[$index]'><p>{{a}}<p><br></div>",
+		check  : "<div ng-repeat='a in q.a'><input type='checkbox' ng-model='answers[q.q][a]'><p>{{a}}<p><br></div> "
 	}
 
 	$scope.addAns = function (a) {
